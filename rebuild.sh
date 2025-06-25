f3412ab12405e06ea9ead57d3895a62a42574f39
MythEclipse <asepharyanalm001@gmail.com>
2025-06-26 01:23:36 +0700

    chore: update dependencies and improve rebuild script
    - Added new dependencies: react-native-animatable, react-native-fs, react-native-svg, and yup.
    - Updated react-native-reanimated and @babel/preset-env to their latest versions.
    - Added babel-jest to devDependencies.
    - Modified rebuild.sh to remove node_modules and lock files before reinstalling dependencies, commented out the Android build commands for clarity.

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,8 @@
-cd android
-./gradlew clean
-cd ..
-npx react-native run-android
+rm -rf node_modules
+rm -f package-lock.json yarn.lock
+# rm -rf app/build
+npm install
+# cd android
+# ./gradlew clean
+# cd ..
+# npx react-native run-android
